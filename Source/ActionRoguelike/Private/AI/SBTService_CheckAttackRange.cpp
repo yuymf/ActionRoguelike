b5463b218101c8bfb35ffc418f94f270fb806742
yuymf <1352948945@qq.com>
2022-12-14 21:10:54 +0800

    #10 AIbasic(Controller & BT & BB & BTservice)

---
+++ Source/ActionRoguelike/Private/AI/SBTService_CheckAttackRange.cpp
@@ -0,0 +1,35 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AI/SBTService_CheckAttackRange.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "AIController.h"
+
+void USBTService_CheckAttackRange::TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds)
+{
+	Super::TickNode(OwnerComp, NodeMemory, DeltaSeconds);
+
+	UBlackboardComponent* BlackBoardComp = OwnerComp.GetBlackboardComponent();
+	if (ensure(BlackBoardComp))
+	{
+		AActor* TargetActor = Cast<AActor>(BlackBoardComp->GetValueAsObject("TargetActor"));
+		if (TargetActor)
+		{
+			AAIController* MyController = OwnerComp.GetAIOwner();
+			if (ensure(MyController))
+			{
+				APawn* AIPawn = MyController->GetPawn();
+				if (ensure(AIPawn))
+				{
+					float DistanceTo = FVector::Distance(TargetActor->GetActorLocation(), AIPawn->GetActorLocation());
+					// check the distance between AI & player
+					bool bWithinRange = DistanceTo < 2000.0f;
+					// check if AI see player
+					bool bHasLOS = bWithinRange ? MyController->LineOfSightTo(TargetActor) : false;
+
+					BlackBoardComp->SetValueAsBool(AttackRangeKey.SelectedKeyName, (bWithinRange && bHasLOS));
+				}
+			}
+		}
+	}
+}
